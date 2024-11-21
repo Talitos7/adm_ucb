c45de1b9d2985c0c0aeca1b2dbb05cd97149d183
luwu <lucivp22@gmail.com>
2024-11-21 02:50:01 -0400

    Headers de Seguridad

--- src/servicios/Addperfil.php
+++ src/servicios/Addperfil.php
@@ -1,10 +1,11 @@
 <?php
-header("Access-Control-Allow-Origin: https://localhost:3000"); // Cambia por tu dominio
+header("Access-Control-Allow-Origin: http://localhost:3000"); // Cambia por tu dominio
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
 include 'conexion.php';
 include 'middleware.php';
+include 'headers.php';
 
 // Validar límite de solicitudes
 checkRateLimit($conn, $_SERVER['REMOTE_ADDR']);
--- src/servicios/uploadPhoto.php
+++ src/servicios/uploadPhoto.php
@@ -1,10 +1,10 @@
 <?php
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: https://localhost:3000"); // Cambia por tu dominio
+header("Access-Control-Allow-Origin: http://localhost:3000"); // Cambia por tu dominio
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type");
 include 'middleware.php';
-
+include 'headers.php';
 // Validar límite de solicitudes
 checkRateLimit($conn, $_SERVER['REMOTE_ADDR']);
 
