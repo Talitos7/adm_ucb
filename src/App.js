0ce6ac016d31587757495b31f9c9da6b62f349ba
luwu <lucivp22@gmail.com>
2024-11-23 18:24:48 -0400

    Merge rama josh en rama PruebaMerge3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import DynamicNavbar from './components/DynamicNavbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Importación única
+import Navbar from './components/Navbar';
+import NavbarAdmin from './components/navbaradmin';
 import Pasantias from './views/pasantias';
 import Perfil from './views/perfil';
 import CarouselSection from './components/CarouselSection';
@@ -14,14 +15,13 @@ import PublicationsAlumni from './views/PublicationsAlumni';
 import PublicationsSociedad from './views/PublicationsSociedad';
 import MallaCurricular from './views/MallaCurricular';
 import LoginPage from './views/LoginPage';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import './App.css'; 
 import EmpresasAliadas from './views/EmpresasAliadas';
 import EmpresasAliadasAdmi from './views/EmpresasAliadasAdmi';
 import UserRegistrationForm from './views/UserRegistrationForm';
 import AdminPublications from './views/AdminPublications';
-import EventosView from './views/EventosView'; // Asegúrate de que la ruta sea correcta
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import './App.css';
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -46,17 +46,14 @@ function App() {
     <Router>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        {/* Navbar dinámico */}
-        <DynamicNavbar darkMode={darkMode} handleThemeChange={handleThemeChange} />
         <Routes>
-          {/* Página de login */}
-          <Route path="/login" element={<LoginPage />} />
-
+        <Route path="/login" element={<LoginPage />} />
           {/* Página principal */}
           <Route
             path="/"
             element={
               <>
+                <Navbar darkMode={darkMode} handleThemeChange={handleThemeChange} />
                 <CarouselSection />
                 <DocenteSection />
                 <InfoSec/>
@@ -67,31 +64,98 @@ function App() {
           />
 
           {/* Página de administración */}
-          <Route path="/admin" element={<AdminPage />} />
+          <Route
+            path="/admin"
+            element={
+              <>
+                <NavbarAdmin darkMode={darkMode} handleThemeChange={handleThemeChange} />
+                <AdminPage />
+              </>
+            }
+          />
 
           {/* Página de pasantías */}
-          <Route path="/pasantias" element={<Pasantias />} />
+          <Route
+            path="/pasantias"
+            element={
+              <>
+                <NavbarAdmin darkMode={darkMode} handleThemeChange={handleThemeChange} />
+                <Pasantias />
+              </>
+            }
+          />
 
-          {/* Publicaciones */}
-          <Route path="/PublicationsIntercambio" element={<PublicationsIntercambio />} />
-          <Route path="/PublicationsSociedad" element={<PublicationsSociedad />} />
-          <Route path="/PublicationsAlumni" element={<PublicationsAlumni />} />
+          {/* Página de publicaciones */}
+          <Route
+            path="/PublicationsIntercambio"
+            element={
+              <>
+                <NavbarAdmin darkMode={darkMode} handleThemeChange={handleThemeChange} />
+                <PublicationsIntercambio darkMode={darkMode} />
+              </>
+            }
+          />
+          <Route
+            path="/PublicationsSociedad"
+            element={
+              <>
+                <NavbarAdmin darkMode={darkMode} handleThemeChange={handleThemeChange} />
+                <PublicationsSociedad darkMode={darkMode} />
+              </>
+            }
+          />
+           <Route
+            path="/PublicationsAlumni"
+            element={
+              <>
+                <NavbarAdmin darkMode={darkMode} handleThemeChange={handleThemeChange} />
+                <PublicationsAlumni darkMode={darkMode} />
+              </>
+            }
+          />
+          <Route
+            path="/UserRegistrationForm"
+            element={
+              <>
+                <NavbarAdmin darkMode={darkMode} handleThemeChange={handleThemeChange} />
+                <UserRegistrationForm darkMode={darkMode} />
+              </>
+            }
+          />
+          <Route path="/MallaCurricular" element={<MallaCurricular darkMode={darkMode} />} />
 
-          {/* Registro de usuario */}
-          <Route path="/UserRegistrationForm" element={<UserRegistrationForm />} />
-
-          {/* Empresas */}
-          <Route path="/empresas" element={<EmpresasAliadas />} />
-          <Route path="/empresasAdmi" element={<EmpresasAliadasAdmi />} />
-
-          {/* Publicaciones de administración */}
-          <Route path="/AdminPublications" element={<AdminPublications />} />
-
-          {/* Eventos */}
-          <Route path="/eventos" element={<EventosView />} />
-
-          {/* Perfil del usuario */}
-          <Route path="/perfil" element={<Perfil />} />
+          {/* Ruta para la página de Pasantías */}
+          <Route path="/pasantias" element={
+          <>
+          <NavbarAdmin darkMode={darkMode} handleThemeChange={handleThemeChange}/> {/* NavbarAdmin */}
+            <Pasantias /> {/* Componente que renderiza la vista de pasantías */}
+          </>
+          } />
+          <Route path="/empresas" element={
+          <>
+          <NavbarAdmin darkMode={darkMode} handleThemeChange={handleThemeChange}/> {/* NavbarAdmin */}
+            <EmpresasAliadas />
+          </>
+          } />
+          <Route path="/empresasAdmi" element={
+          <>
+          <NavbarAdmin darkMode={darkMode} handleThemeChange={handleThemeChange}/> {/* NavbarAdmin */}
+            <EmpresasAliadasAdmi />
+          </>
+          } />
+          <Route path="/AdminPublications" element={
+          <>
+          <NavbarAdmin darkMode={darkMode} handleThemeChange={handleThemeChange}/> {/* NavbarAdmin */}
+            <AdminPublications />
+          </>
+          } />
+        {/* Ruta para la página de Perfil */}
+          <Route path="/perfil" element={
+          <>
+          <NavbarAdmin darkMode={darkMode} handleThemeChange={handleThemeChange}/> {/* NavbarAdmin */}
+            <Perfil /> {/* Componente que renderiza la vista de pasantías */}
+          </>
+        } />
         </Routes>
       </ThemeProvider>
     </Router>
