979961243be90e73f94d6347ce2eb39e3d755cfd
AndyLaffertt <alaffertt@gmail.com>
2024-11-24 23:08:19 -0400

    detalles del modal

--- src/components/AdminPublicationModal.js
+++ src/components/AdminPublicationModal.js
@@ -5,7 +5,7 @@ import CloseIcon from '@mui/icons-material/Close'; // Icono de cierre (opcional)
 const AdminPublicationModal = ({ publication, onClose, darkMode }) => {
   if (!publication) return null; // Si no hay publicación seleccionada, no se renderiza el modal
 
-  const { titulo, detalle, categoria, multimedia } = publication;
+  const { titulo,descripcionpublicacion, categoria, multimedia, autor, usuario_emailadm } = publication;
 
   return (
     <div className={`modal-overlay ${darkMode ? 'dark-mode' : ''}`} onClick={onClose}>
@@ -20,7 +20,15 @@ const AdminPublicationModal = ({ publication, onClose, darkMode }) => {
           <h2>{titulo}</h2>
         </div>
         <div className="modal-body">
-          <p>{detalle}</p>
+          <p>
+            <strong>Autor:</strong> {autor}
+          </p>
+          <p>
+            <strong>Correo:</strong> {usuario_emailadm}
+          </p>
+          <p>
+            <strong>Descripción:</strong> {descripcionpublicacion}
+          </p>
           <p>
             <strong>Categoría:</strong> {categoria}
           </p>
